3ea4ea00a5214006e5cf01d4d009d3d7c1a6f710
Josh <joshapps027@gmail.com>
2020-02-14 19:04:09 -0700

    initial commit

--- main.js
+++ main.js
@@ -35,6 +35,26 @@ fs.readdir("./database/", (err, folders) => {
 	if (err) throw err;
 
 	for (let i = 0; i < folders.length; i++) {
+		if(folders[i] = 'servers') {
+				fs.readdir("./database/servers", (err, servers) => {
+					for (let i = 0; i < servers.length; i++) {
+						fs.readdir(`./database/servers/${servers[i]}`, (err, files) => {
+							let jsfiles = files.filter(f => f.split(".").pop() === 'js');
+							if (jsfiles.length < 1) {
+							console.log(`No datamaps in ${folders[i]}`);
+							return;
+							}
+
+							jsfiles.forEach((file) => {
+							let properties = require(`./database/${folders[i]}/${file}`);
+							if(!properties.enabled) return;
+							console.log(`Loaded ${file}`);
+							client.db.set(properties.name, properties);
+			})
+						}) 
+					}
+				})
+		}else {
 		fs.readdir(`./database/${folders[i]}`, (e, files) => {
 			let jsfiles = files.filter(f => f.split(".").pop() === 'js');
 			if (jsfiles.length < 1) {
@@ -49,6 +69,7 @@ fs.readdir("./database/", (err, folders) => {
 				client.db.set(properties.name, properties);
 			})
 		})
+	}
 	}
 })
 
