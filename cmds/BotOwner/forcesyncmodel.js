2ad32579fb8fde0b653b1498b51a542b4a200348
Josh Zakaras <joshapps027@gmail.com>
2020-02-15 10:54:40 -0700

    db management

--- cmds/BotOwner/forcesyncmodel.js
+++ cmds/BotOwner/forcesyncmodel.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const config = require("../../config.json");
 module.exports.run = async (client, msg, args) => {
+    console.log(args);
     client.db.args[0].sync({force: true});
     msg.reply(`Force Synced ${args[0]}`);
 }
